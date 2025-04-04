5e33b98c0227511fdd6abe88050a52401f4a95c5
thomkell <th.h.keller@gmail.com>
2025-04-04 07:22:36 -0700

    added sidebar for year selection

--- app.py
+++ app.py
@@ -13,14 +13,18 @@ filepaths = {
     "2025": f"{path}/2025.xlsx"
 }
 
-# Category Sales
-st.header("Sold Ticket Sales per Category (2025)")
-sold_category_sales, df_sold = get_category_sales(filepaths["2025"])
+# Sidebar for year selection
+st.sidebar.title("Select Year")
+selected_year = st.sidebar.selectbox("Year", list(filepaths.keys()))
+
+# Display sold tickets for the selected year
+st.header(f"Sold Ticket Sales per Category ({selected_year})")
+sold_category_sales, df_sold = get_category_sales(filepaths[selected_year])
 fig_category_sales = plot_category_sales(sold_category_sales, df_sold)
 st.plotly_chart(fig_category_sales)
 
 # Cumulative Sales Comparison
-st.header("Cumulative Ticket Pre-Sales Comparison (December - May)")
+st.header("Cumulative Ticket Pre-Sales Comparison (December - June)")
 sales_data = load_sales_data(filepaths)
 preprocessed_sales_data = preprocess_sales_data(sales_data)
 cumulative_sales_timeline = aggregate_sales_timeline(preprocessed_sales_data)
@@ -29,7 +33,7 @@ st.plotly_chart(fig_cumulative_sales)
 
 # Ticket Sales Locations
 st.header("Ticket Sales Locations")
-ticket_locations = get_ticket_locations(filepaths["2025"])
+ticket_locations = get_ticket_locations(filepaths[selected_year])
 cache_path = "/Users/thomaskeller/Dropbox/RK/Eventfrog/cached_locations.csv"
 ticket_locations_geocoded = geocode_locations(ticket_locations, cache_path)
 fig_ticket_locations = plot_ticket_locations(ticket_locations_geocoded)
@@ -39,6 +43,3 @@ st.plotly_chart(fig_ticket_locations)
 st.header("Tickets Sold Per Location")
 fig_tickets_sold_by_location = plot_tickets_sold_by_location(ticket_locations_geocoded)
 st.plotly_chart(fig_tickets_sold_by_location)
-
-fig = plot_cumulative_sales(cumulative_sales_timeline)
-fig.write_html("dashboard.html", full_html=True)
